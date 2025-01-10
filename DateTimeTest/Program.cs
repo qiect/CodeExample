2c06f02f64e8d6fc5b55519d8a53bb0a1219e44f
Chet <qctvip@vip.163.com>
2025-01-10 16:35:59 +0800

    feat: 新增时间相关的测试应用

---
+++ DateTimeTest/Program.cs
@@ -0,0 +1,11 @@
+﻿/**
+ * 时间日期测试
+ */
+
+
+// 计算相差时间
+var now = DateTime.Now;
+var yesterday = DateTime.Now.AddDays(-1);
+var time = now.Subtract(yesterday).TotalMinutes / 60;
+Console.WriteLine($"昨天是{yesterday.ToLongTimeString()}, 今天是{now.ToLongTimeString()}, 经过了{time.ToString("F2")}小时");
+Console.ReadLine();
